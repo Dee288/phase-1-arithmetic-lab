484648e1cef93709b7b96352ff3a907cbbab5d71
dee288 <dianamajala10@gmail.com>
2022-06-10 05:49:03 +0300

    commit tree 35eea2ee75b5351470fd2c1d2a9a88c99298d62d

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 // Write your code here
-const num1 = 2;
-const num2 = 31;
+const num1 = 31;
+const num2 = 2;
 
 const multiply = num1 * num2;
 console.log()
 
-const random = Math.floor( Math.random() * 1000 ) + 1;
+const random = Math.floor( Math.random() * 10 ) + 1;
 const num3 = 16;
 const num4 = 6;
 const mod = num3 % num4;
 
-const max = Math.max( 5, 10, 15, 20 );
+const max = Math.max( 11, 14, 18, 20 );
